731144ff0f7f2cf2cac3dcf5375f8a027b813ad8
EC2 Default User <ec2-user@ip-172-31-15-40.eu-central-1.compute.internal>
2018-01-21 06:52:28 +0000

    Exercise 1

---
+++ variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+firstname = "Firstname"
+lastname = "Lastname"
+age = 95
+birthdate = "01/01/1901"
+
+print ("My name is %s %s" % (firstname, lastname))
+print ("I was born on %s and my age is %s" % (birthdate, age))
