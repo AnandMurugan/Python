56765e04a72012312751fd217263452ce9dff9d6
Ananda Sentraya Perumal <m.anandsp@gmail.com>
2018-01-22 20:21:21 +0100

    cosmetic change

--- functions_test.py
+++ functions_test.py
@@ -3,11 +3,11 @@
 def print_message(message, count):
     while count > 0:
         print (message)
-        count = count - 1
+        count -= 1
 
 
 message = raw_input("Please enter a message to echo ")
-string_count = raw_input("Please enter number of times to print the message ") 
+string_count = raw_input("Please enter number of times to print the message ").strip() 
 
 if string_count:
     count = int(string_count)
