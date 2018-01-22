504316e9c45ddbe38f2b0ab0329b4bcd4ce6c5ef
EC2 Default User <ec2-user@ip-172-31-15-40.eu-central-1.compute.internal>
2018-01-22 15:29:05 +0000

    Updated function - 1st version

--- functions_test.py
+++ functions_test.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-def readuserinput():
-    msg = raw_input("Please enter a message to echo ")
-    return (msg)
+def print_message(message, count):
+    while count > 0:
+        print (message)
+        count = count - 1
 
-message = readuserinput()
+
+message = raw_input("Please enter a message to echo ")
+count = input("Please enter number of times to print the message ")
+
+if not count:
+    count = 1
+
+print_message(message, count)
 print ("You entered : %s " % (message))
