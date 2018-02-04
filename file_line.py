0fec380ea56cf68b5f16e8a80f5344f62a5b5d34
EC2 Default User <ec2-user@ip-172-31-15-40.eu-central-1.compute.internal>
2018-02-04 17:34:59 +0000

    adapted to solution

--- file_line.py
+++ file_line.py
@@ -4,20 +4,19 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description="Program to retrieve a specified line from a file")
-parser.add_argument('filename',help='Specify a File name to read contents.')
-parser.add_argument('--linenumber', type=int, help='line number in the file to return')
+parser.add_argument('filename', help='Specify a File name to read contents.')
+parser.add_argument('line_number', type=int, help='line number in the file to return')
 
 args = parser.parse_args()
 
 try:
     f = open(args.filename, 'r')
+    with f:
+        lines = f.readlines()
+        line = lines[args.line_number - 1]
+except IndexError:
+    print("Line number %s not found" % (args.line_number))
 except IOError:
     print("IOError: %s doesn't exist" % (args.filename))
 else:
-    with f:
-        lines=f.readlines()
-        line_to_return = args.linenumber
-        if len(lines)<line_to_return:
-            print ("Line number %s not found" % (line_to_return))
-        else:
-            print lines[line_to_return-1].strip()
+    print line.strip()
