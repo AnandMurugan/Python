23fdb2baa5d72191e0db27e4226d3eb2498da43c
EC2 Default User <ec2-user@ip-172-31-15-40.eu-central-1.compute.internal>
2018-02-04 17:25:46 +0000

    working solution to retrieve a line in a file

--- file_line.py
+++ file_line.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Program to retrieve a specified line from a file")
 parser.add_argument('filename',help='Specify a File name to read contents.')
+parser.add_argument('--linenumber', type=int, help='line number in the file to return')
 
 args = parser.parse_args()
 
-with open(args.filename,'r') as f:
-    lines=f.readlines()
-    print lines
-
-print('EOF')
+try:
+    f = open(args.filename, 'r')
+except IOError:
+    print("IOError: %s doesn't exist" % (args.filename))
+else:
+    with f:
+        lines=f.readlines()
+        line_to_return = args.linenumber
+        if len(lines)<line_to_return:
+            print ("Line number %s not found" % (line_to_return))
+        else:
+            print lines[line_to_return-1].strip()
